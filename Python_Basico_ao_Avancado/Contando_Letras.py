2e7a26af7892e1d4c201844359ca0686c371f60f
guPego <gustavoprg04@gmail.com>
2023-10-10 10:47:49 -0300

    Contandos letras.

---
+++ Python_Basico_ao_Avancado/Contando_Letras.py
@@ -0,0 +1,26 @@
+frase = 'Python é uma linguagem de programação de alto nível, interpretada de script, imperativa, orientada a objetos, funcional, de tipagem dinâmica e forte.'
+
+i = 0
+quantidade_letra_mais_repetida = 0
+letra_mais_repetida = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    quantidade_letra_mais_repetida_atual = frase.count(letra_atual)
+
+    if quantidade_letra_mais_repetida < quantidade_letra_mais_repetida_atual:
+        quantidade_letra_mais_repetida = quantidade_letra_mais_repetida_atual
+        letra_mais_repetida = letra_atual
+
+    i += 1
+
+print(
+    'A letra que apareceu mais vezes foi '
+    f'"{letra_mais_repetida}" que apareceu '
+    f'{quantidade_letra_mais_repetida}x'
+)
--- Python_Basico_ao_Avancado/aula1.py
+++
@@ -1,9 +0,0 @@
-frase = "Gustavo e Isadora pra sempre juntos."
-
-i = 0;
-while i < len(frase):
-  letra_atual = frase[i];
-  quantas_vezes_letra_apareceu = frase.count(letra_atual);
-
-  print(letra_atual, quantas_vezes_letra_apareceu)
-  i+=1;
