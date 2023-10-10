d3ecf5352a8445324247feb77fc2380a88660702
guPego <gustavoprg04@gmail.com>
2023-10-10 10:42:23 -0300

    Contados de Letras

--- Python_Basico_ao_Avancado/aula1.py
+++ Python_Basico_ao_Avancado/aula1.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+frase = "Gustavo e Isadora pra sempre juntos."
+
+i = 0;
+while i < len(frase):
+  letra_atual = frase[i];
+  quantas_vezes_letra_apareceu = frase.count(letra_atual);
+
+  print(letra_atual, quantas_vezes_letra_apareceu)
+  i+=1;
